4406040eb179944eca0dab497a8237a1d01509af
lkq123456 <44870344+lkq123456@users.noreply.github.com>
2019-02-03 23:14:23 +0800

    Update 基础题.cpp

--- 基础题.cpp
+++ 基础题.cpp
@@ -2,11 +2,11 @@
 #include<stdlib.h>
 #include<string.h>
 struct a{
-	char in[7];
-	char out[7];
+	char in[7];                    //a 结构体是用于读入所有信息   in是发信人 out是接受人 m是信息量
+	char out[7];                         
 	int m;
 };
-struct b{
+struct b{                              //b黑客结构体
 	char name[7];
 };
 int main()
@@ -14,7 +14,7 @@ int main()
 	int i=0,c,t=0,j,flag=0,n=0;
 	struct a fa[10000];
 	struct b he[100];
-	FILE* fp=fopen("Request.txt","r");
+	FILE* fp=fopen("Request.txt","r");                                    //读文件存信息
 	while(3==fscanf(fp,"%s %s %d",&fa[i].in,&fa[i].out,&fa[i].m)) i++;
 	c=i-1;
 	fclose(fp);
@@ -22,23 +22,16 @@ int main()
 	{
 		for(j=0;j<c;j++)				
 		{
-			if(strcmp(fa[i].in ,fa[j].in)==0)	t=t+fa[j].m;
+			if(strcmp(fa[i].in ,fa[j].in)==0)	t=t+fa[j].m;        //将同一个发信人的信息量累加，t初始值为0因此j从0开始
 		}
-		if(t>1500)
+		if(t>1500)                         //判断信息量是否超标
 		{	
 			for(j=0;j<=n;j++)						
 			{
-				if(strcmp(fa[i].in,he[j].name)==0)	
-				{
-					flag=1;
-					break;	
-				}
+			if(strcmp(fa[i].in,he[j].name)==0)	{flag=1;break;}    //同一个人 从头便利的话会被重复算入黑客，因此我们对已被定位黑客的人标记使再遇到时不会重复计算
 			}
 			if(!flag)								
-			{
-				strcpy(he[n].name,fa[i].in);
-				n++;
-			}
+			{strcpy(he[n].name,fa[i].in);n++;}
 			else flag=0;
 		}	
 		t=0;
